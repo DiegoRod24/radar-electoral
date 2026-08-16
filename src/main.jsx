ce5a5e094cc48cd77806937c15d35bf96ffae6d6
DiegoRod24 <99710581+DiegoRod24@users.noreply.github.com>
2026-08-16 18:48:27 -0500

    Agrega entrada React

---
+++ src/main.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+
+function App() {
+  return <div><h1>Radar Electoral</h1><p>Entorno inicial listo.</p></div>
+}
+
+createRoot(document.getElementById('root')).render(<App />)
